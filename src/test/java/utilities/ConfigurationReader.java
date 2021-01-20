fbabb89bff8b3990fba44ff7efc0cd3ec2eab7d3
avyunusemre <deu.av.yunusemre@gmail.com>
2021-01-20 03:04:32 +0100

    second commit

---
+++ src/test/java/utilities/ConfigurationReader.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    static Properties properties;
+    static{
+        String path = "configuration.properties";
+
+        try {
+            FileInputStream file = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(file);
+        } catch (Exception e) {
+
+        }
+    }
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+}
